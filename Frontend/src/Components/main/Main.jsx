38163e1b306d9c5901eb27e5766e96a5cc3a7772
Abdullah <liten30880@wwgoc.com>
2023-10-10 23:55:08 +0200

    first commit

---
+++ Frontend/src/Components/main/Main.jsx
@@ -0,0 +1,239 @@
+import { AddShoppingCartOutlined, Close } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Container,
+  Dialog,
+  IconButton,
+  Rating,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { useState } from "react";
+import ProductDetails from "./ProductDetails";
+
+// import { useSelector, useDispatch } from 'react-redux'
+import { useGetProductsAPIByNameQuery } from "../../Redux/productsAPI";
+import { AnimatePresence, motion } from "framer-motion";
+
+const Main = () => {
+  const allProductsAPI = "products?populate=*";
+  const menCategoryAPI = "products?populate=*&filters[category][$eq]=men";
+  const womenCategoryAPI = "products?populate=*&filters[category][$eq]=women";
+
+  const [myData, setMyData] = useState(allProductsAPI);
+  const { data, error, isLoading } = useGetProductsAPIByNameQuery(myData);
+  const [clickedProduct, setClickedProduct] = useState({});
+
+  const theme = useTheme();
+
+  const handleAlignment = (_event, newValue) => {
+    if (newValue !== null) {
+      setMyData(newValue);
+    }
+    
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  if (isLoading) {
+    return (
+      <Box sx={{ py: 11, textAlign:"center"}}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+    <Container sx={{py:11, textAlign: "center"}}>
+      <Typography variant="h6">{error.
+// @ts-ignore
+      error}</Typography>
+      
+      <Typography variant="h6" >Please try again later</Typography>
+    </Container>
+    
+    );
+  }
+
+  if (data) {
+    console.log(data)
+    return (
+      <Container sx={{ py: 9 }}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexWrap={"wrap"}
+          gap={3}
+        >
+          <Box>
+            <Typography variant="h6">Selected Products</Typography>
+            <Typography fontWeight={300} variant="h6">
+              All our new arrivals in a exclusive brand selection
+            </Typography>
+          </Box>
+
+          <ToggleButtonGroup
+            color="error"
+            value={myData}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+            sx={{
+              ".Mui-selected": {
+                border: "1px solid rgba(233, 69, 96, 0.5) !important",
+                color: "#e94560",
+                backgroundColor: "initial",
+              },
+            }}
+          >
+            <ToggleButton
+              sx={{ color: theme.palette.text.primary }}
+              className="myButton"
+              value={allProductsAPI}
+              aria-label="left aligned"
+            >
+              All Products
+            </ToggleButton>
+            <ToggleButton
+              sx={{ mx: "16px !important", color: theme.palette.text.primary }}
+              className="myButton"
+              value={menCategoryAPI}
+              aria-label="centered"
+            >
+              MEN category
+            </ToggleButton>
+            <ToggleButton
+              sx={{ color: theme.palette.text.primary }}
+              className="myButton"
+              value={womenCategoryAPI}
+              aria-label="right aligned"
+            >
+              WOMEN category
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Stack>
+
+        <Stack
+          direction={"row"}
+          flexWrap={"wrap"}
+          justifyContent={"space-between"}
+        >
+
+          <AnimatePresence>
+          {data.data.map((item) => {
+            return (
+              <Card
+              component={motion.section}
+              layout
+              initial={{ transform: "scale(0)"}}
+              animate={{transform: "scale(1)"}}
+              transition={{duration: 0.6, type: "spring", stiffness: 50,}}
+                key={item.id}
+                sx={{
+                  maxWidth: 333,
+                  mt: 6,
+                  ":hover .MuiCardMedia-root ": {
+                    rotate: "1deg",
+                    scale: "1.1",
+                    transition: "0.35s",
+                  },
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 277 }}
+                  // @ts-ignore
+                  image={`${item.attributes.productImg.data[0].attributes.url}`}
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Stack
+                    direction={"row"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                  >
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.attributes.productTitle}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.attributes.productPrice}
+                    </Typography>
+                  </Stack>
+                  <Typography variant="body2" color="text.secondary">
+                    {item.attributes.productDescription}
+                  
+                  </Typography>
+                </CardContent>
+
+                <CardActions sx={{ justifyContent: "space-between" }}>
+                  <Button
+                    onClick={() => {
+                      handleClickOpen()
+                      setClickedProduct(item)
+                    }}
+                    sx={{ textTransform: "capitalize" }}
+                    size="large"
+                  >
+                    <AddShoppingCartOutlined sx={{ mr: 1 }} fontSize="small" />
+                    add to cart
+                  </Button>
+                  <Button size="small">
+                    <Rating
+                      name="read-only"
+                      value={item.attributes.productRating}
+                      readOnly
+                    />
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          })}
+          </AnimatePresence>
+        
+        </Stack>
+
+        <Dialog
+          sx={{ ".MuiPaper-root": { minWidth: { xs: "100%", md: 800 } } }}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <IconButton
+            sx={{
+              ":hover": { color: "red", rotate: "180deg", transition: "0.3s" },
+              position: "absolute",
+              top: 0,
+              right: 10,
+            }}
+            onClick={handleClose}
+          >
+            <Close />
+          </IconButton>
+
+          <ProductDetails clickedProduct={clickedProduct} />
+        </Dialog>
+      </Container>
+    );
+  }
+};
+
+export default Main;
